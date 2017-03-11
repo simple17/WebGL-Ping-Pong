33d75e2f2ac3f3ec6dd8d6fc90b2f795043029d1
greyHorse <flerris.m@gmail.com>
2017-03-11 19:04:51 +0300

    added spotlight movement

--- public/main.js
+++ public/main.js
@@ -53,7 +53,7 @@ function foo() {
     //первый куб с размерами, задаём цвет
     var cubeGeometry = new THREE.CubeGeometry(vars.sizeOfPlayers,2,0);
     var cubeMaterial = new THREE.MeshBasicMaterial(
-        {color: 0x6C8995, wireframe: false});
+        {color: 0xe9211a, wireframe: false});
     var cubeFirst = new THREE.Mesh(cubeGeometry, cubeMaterial);
     //cube.rotation.z = -0.5*Math.PI;
     cubeFirst.position.x = players.first.startPosition.x;
@@ -65,7 +65,7 @@ function foo() {
     //второй куб
     var cubeGeometry = new THREE.CubeGeometry(vars.sizeOfPlayers,2,0);
     var cubeMaterial = new THREE.MeshBasicMaterial(
-        {color: 0x1b243b, wireframe: false});
+        {color: 0x365023, wireframe: false});
     var cubeSecond = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cubeSecond.rotation.z = 45;
     cubeSecond.position.x = players.second.startPosition.x;
@@ -77,7 +77,7 @@ function foo() {
     //третий куб
     var cubeGeometry = new THREE.CubeGeometry(vars.sizeOfPlayers,2,0);
     var cubeMaterial = new THREE.MeshBasicMaterial(
-        {color: 0x425b4d, wireframe: false});
+        {color: 0x00accf, wireframe: false});
     var cubeThird = new THREE.Mesh(cubeGeometry, cubeMaterial);
     cubeThird.rotation.z = -45;
     cubeThird.position.x = players.third.startPosition.x;
@@ -97,17 +97,17 @@ function foo() {
     vars.SCENE.add(sphere);
 
     //создаём источник света
-    var spotLight = new THREE.SpotLight( 0xffffff );
-    spotLight.position.set( 15, 5, 0 );
-    vars.SCENE.add(spotLight);
+    var spotLightLeft = new THREE.SpotLight( 0xffffff );
+    spotLightLeft.position.set( cubeFirst.position.x, cubeFirst.position.y, 0);
+    vars.SCENE.add(spotLightLeft);
 
-    var spotLight2 = new THREE.SpotLight( 0xffffff );
-    spotLight2.position.set( 0, -14, 0 );
-    vars.SCENE.add(spotLight2);
+    var spotLightRight = new THREE.SpotLight( 0xffffff );
+    spotLightRight.position.set( cubeSecond.position.x, cubeSecond.position.y, 0);
+    vars.SCENE.add(spotLightRight);
 
-    var spotLight3 = new THREE.SpotLight( 0xffffff );
-    spotLight3.position.set( -15, 5, 0 );
-    vars.SCENE.add(spotLight3);
+    var spotLightBottom = new THREE.SpotLight( 0xffffff );
+    spotLightBottom.position.set( cubeThird.position.x, cubeThird.position.y, 0);
+    vars.SCENE.add(spotLightBottom);
 
     //задаём положение камеры
     camera.position.x = 0;
@@ -177,20 +177,22 @@ function foo() {
 
 
 
-        //условие перемещения нижнего куба
+        //условие перемещения нижнего куба и источника света
 
         if (players.first.direction == 'left' && cubeFirst.position.x > -vars.sizeOfSideOfTriangle) {
             cubeFirst.position.x -= vars.cubeStep;
+            spotLightLeft.position.x -= vars.cubeStep;
         } else if (players.first.direction == 'left' && cubeFirst.position.x <= -vars.sizeOfSideOfTriangle) {
             players.first.direction = 'right';
         } else if (players.first.direction == 'right' && cubeFirst.position.x <= vars.sizeOfSideOfTriangle) {
             cubeFirst.position.x += vars.cubeStep;
+            spotLightLeft.position.x += vars.cubeStep;
         } else if (players.first.direction == 'right' && cubeFirst.position.x > vars.sizeOfSideOfTriangle) {
             players.first.direction = 'left';
         }
 
 
-        //movement of second player
+        //movement of second player and spotlight
 
         if (players.second.direction == 'left' &&
             cubeSecond.position.x > -vars.sizeOfSideOfTriangle &&
@@ -198,6 +200,9 @@ function foo() {
 
             cubeSecond.position.x -= vars.cubeStep;
             cubeSecond.position.y = Math.sqrt(3) * cubeSecond.position.x + vars.sizeOfSideOfTriangle * (Math.sqrt(3) - 1);
+
+            spotLightRight.position.x -= vars.cubeStep;
+            spotLightRight.position.y = Math.sqrt(3) * cubeSecond.position.x + vars.sizeOfSideOfTriangle * (Math.sqrt(3) - 1);
 
         } else if (players.second.direction == 'left' &&
             cubeSecond.position.x <= -vars.sizeOfSideOfTriangle &&
@@ -212,13 +217,16 @@ function foo() {
             cubeSecond.position.x += vars.cubeStep;
             cubeSecond.position.y = Math.sqrt(3) * cubeSecond.position.x + vars.sizeOfSideOfTriangle * (Math.sqrt(3) - 1);
 
+            spotLightRight.position.x += vars.cubeStep;
+            spotLightRight.position.y = Math.sqrt(3) * cubeSecond.position.x + vars.sizeOfSideOfTriangle * (Math.sqrt(3) - 1);
+
         } else if (players.second.direction == 'right' &&
             cubeSecond.position.x > 0 &&
             cubeSecond.position.y > (Math.sqrt(3) - 1) * vars.sizeOfSideOfTriangle) {
             players.second.direction = 'left';
         }
 
-        //movement of third player
+        //movement of third player and spotlight
 
         if (players.third.direction == 'left' &&
             cubeThird.position.x > 0 &&
@@ -226,6 +234,9 @@ function foo() {
 
             cubeThird.position.x -= vars.cubeStep;
             cubeThird.position.y = -Math.sqrt(3) * cubeThird.position.x + vars.sizeOfSideOfTriangle * (Math.sqrt(3) - 1);
+
+            spotLightBottom.position.x -= vars.cubeStep;
+            spotLightBottom.position.y = -Math.sqrt(3) * cubeThird.position.x + vars.sizeOfSideOfTriangle * (Math.sqrt(3) - 1);
 
         } else if (players.third.direction == 'left' &&
             cubeThird.position.x <= 0 &&
@@ -238,6 +249,9 @@ function foo() {
 
             cubeThird.position.x += vars.cubeStep;
             cubeThird.position.y = -Math.sqrt(3) * cubeThird.position.x + vars.sizeOfSideOfTriangle * (Math.sqrt(3) - 1);
+
+            spotLightBottom.position.x += vars.cubeStep;
+            spotLightBottom.position.y = -Math.sqrt(3) * cubeThird.position.x + vars.sizeOfSideOfTriangle * (Math.sqrt(3) - 1);
 
         } else if (players.third.direction == 'right' &&
             cubeThird.position.x >= vars.sizeOfSideOfTriangle &&
